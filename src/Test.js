06e13b64261fa8825e688eeeca16bb201547188f
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-05 08:49:13 +0530

    React pet

--- src/Test.js
+++ src/Test.js
@@ -5,15 +5,21 @@ import Ptag from "./Ptag";
 import Footer from "./Footer";
 import Pet from "./Pet";
 
+const pets = [
+  { name: "cat", age: "12", id: "1" },
+  { name: "dog", age: "12", id: "2" },
+  { name: "fox", age: "12", id: "3" },
+  { name: "roger", age: "10", id: "4" },
+];
 export default function Test() {
   return (
     <div>
       <Htag name="ddd" age="23"></Htag>
       <Ptag name="ddd" age="23"></Ptag>
       <ul>
-        <Pet name="cat" age="23"></Pet>
-        <Pet name="dog" age="12"></Pet>
-        <Pet name="mouse" age="10"></Pet>
+        {pets.map((pet) => {
+          return <Pet name={pet.name} age={pet.age} key={pet.id}></Pet>;
+        })}
       </ul>
       <Footer name="ddd" age="23">
         {" "}
